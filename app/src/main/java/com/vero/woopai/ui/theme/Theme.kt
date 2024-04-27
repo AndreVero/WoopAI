8333a19150304398b0148b75fe0e22e63e3052ce
Andrii <andriyveremiyenko@gmail.com>
2024-04-27 13:09:39 +0100

    Change color pallete

--- app/src/main/java/com/vero/woopai/ui/theme/Theme.kt
+++ app/src/main/java/com/vero/woopai/ui/theme/Theme.kt
@@ -16,25 +16,8 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = AccentColor,
+    background = BackgroundColor
 )
 
 @Composable
@@ -50,8 +33,7 @@ fun WoopAITheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
